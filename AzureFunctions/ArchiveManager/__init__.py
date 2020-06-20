696d640b35964f49640ebc82eb89c71a31002474
Szymon <ctk7000@gmail.com>
2020-06-20 19:41:08 +0200

    saving statistics in db

---
+++ AzureFunctions/ArchiveManager/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+import azure.functions as func
+
+import os
+from .ArchiveService import ArchiveService
+
+
+
+def main(mytimer: func.TimerRequest, outdoc: func.Out[func.Document]):
+    logging.info("Executing Archive Manager")
+    
+    # arr = os.listdir()
+    # logging.info(arr)
+    # import pathlib
+    # logging.info(pathlib.Path().absolute())
+    service = ArchiveService()
+    service.execute()
+
+
+if __name__ == '__main__':
+    main(func.TimerRequest, 0)
+
+
+
--- AzureFunctions/DataCrawler/__init__.py
+++ AzureFunctions/DataCrawler/__init__.py
@@ -8,12 +8,12 @@ from .CrawlerService import CrawlerService
 
 def main(mytimer: func.TimerRequest, outdoc: func.Out[func.Document]):
     logging.info("Executing Data Crawler")
-    arr = os.listdir()
-    logging.info(arr)
-    import pathlib
-    logging.info(pathlib.Path().absolute())
-    service = CrawlerService()
-    service.execute()
+    # arr = os.listdir()
+    # logging.info(arr)
+    # import pathlib
+    # logging.info(pathlib.Path().absolute())
+    # service = CrawlerService()
+    # service.execute()
 
 
 if __name__ == '__main__':

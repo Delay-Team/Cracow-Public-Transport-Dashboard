dcd65be0585595d68536b4633d5d3c2956693996
Szymon <ctk7000@gmail.com>
2020-06-27 13:18:40 +0200

    addes restapi for line stats

--- RestApi/src/routes/statistics.js
+++ RestApi/src/routes/statistics.js
@@ -9,7 +9,7 @@ router.get('/lines', async(request, response) => {
         let res = [];
         for (let stat in stats) {
             var tempStat = {};
-            const dict = convert(stats[stat].avg_delay_line_statistics)
+            const dict = convertLine(stats[stat].avg_delay_line_statistics)
             dict.sort((a, b) => (a.avgDelay < b.avgDelay) ? 1 : -1)
             tempStat["delays"] = dict;
             tempStat["date"] = stats[stat].timestamp;
@@ -23,11 +23,41 @@ router.get('/lines', async(request, response) => {
 
 });
 
-function convert(obj) {
+router.get('/stops', async(request, response) => {
+
+    try {
+        const stats = await LineStatistics.find();
+        let res = [];
+        for (let stat in stats) {
+            var tempStat = {};
+            if (stats[stat].avg_delay_stop_statistics == undefined) continue
+            const dict = convertStop(stats[stat].avg_delay_stop_statistics)
+            dict.sort((a, b) => (a.avgDelay < b.avgDelay) ? 1 : -1)
+            tempStat["delays"] = dict;
+            tempStat["date"] = stats[stat].timestamp;
+            res.push(tempStat);
+        }
+        response.json(res);
+
+    } catch (error) {
+        response.json(error);
+    }
+
+});
+
+function convertLine(obj) {
     return Object.keys(obj).map(key => ({
         line: key,
         avgDelay: obj[key]
     }));
 }
+
+function convertStop(obj) {
+    return Object.keys(obj).map(key => ({
+        stop: key,
+        avgDelay: obj[key]
+    }));
+}
+
 
 module.exports = router;

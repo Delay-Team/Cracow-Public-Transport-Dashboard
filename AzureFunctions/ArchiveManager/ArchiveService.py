e353d08018cc7ba97fc3d0b853a75e75e5fe905f
Szymon <ctk7000@gmail.com>
2020-06-27 13:06:52 +0200

    working saving stops statistcs to db

--- AzureFunctions/ArchiveManager/ArchiveService.py
+++ AzureFunctions/ArchiveManager/ArchiveService.py
@@ -19,45 +19,54 @@ class ArchiveService:
         stop_result = {}
         for stop in all_stops:
             print("Current stop" + stop) 
-            lineTrips = self.tripRepository.get_trips_for_stop(stop, currentDay)
+            stopTrips = self.tripRepository.get_trips_for_stop(stop, currentDay)
             delaySum = datetime.strptime("00:00", FMT)
-            for trip in lineTrips:
+            for trip in stopTrips:
                 planned_time = trip["planned_time"]
                 actual_time = trip["actual_time"]
                 tdelta = datetime.strptime(actual_time, FMT) - datetime.strptime(planned_time, FMT)
                 delaySum = delaySum + tdelta
             print("DelaySum: ")
             print(delaySum)  
+            sumInSeconds = float(delaySum.hour * 60 * 60 + delaySum.minute * 60)
+            if (len(stopTrips) == 0):
+                avgDelayInSeconds = 0
+            else:
+                avgDelayInSeconds = float(sumInSeconds) / float(len(stopTrips))
+            
             secondsToAdd = timedelta(seconds=avgDelayInSeconds)
             secondsToAddInDatetime = datetime(2000, 1, 1, 0, 0, 0) + secondsToAdd
             HMSFormat='%H:%M:%S'
-            stop_result[line] = secondsToAddInDatetime.strftime(HMSFormat)          
+            stop_name_trimmed = stop.replace('.', "")
+            stop_result[stop_name_trimmed] = secondsToAddInDatetime.strftime(HMSFormat)          
 
-        # allLines = self.tripRepository.get_all_lines(currentDay)
-        # print("AllLines")
-        # print(allLines)
-        # result = {}
+        allLines = self.tripRepository.get_all_lines(currentDay)
+        print("AllLines")
+        print(allLines)
+        result = {}
 
-        # for line in allLines:
-        #     print("Current Line" + line) 
-        #     lineTrips = self.tripRepository.get_trips_for_line(line, currentDay)
-        #     delaySum = datetime.strptime("00:00", FMT)
-        #     for trip in lineTrips:
-        #         planned_time = trip["planned_time"]
-        #         actual_time = trip["actual_time"]
-        #         tdelta = datetime.strptime(actual_time, FMT) - datetime.strptime(planned_time, FMT)
-        #         delaySum = delaySum + tdelta
+        for line in allLines:
+            print("Current Line" + line) 
+            lineTrips = self.tripRepository.get_trips_for_line(line, currentDay)
+            delaySum = datetime.strptime("00:00", FMT)
+            for trip in lineTrips:
+                planned_time = trip["planned_time"]
+                actual_time = trip["actual_time"]
+                tdelta = datetime.strptime(actual_time, FMT) - datetime.strptime(planned_time, FMT)
+                delaySum = delaySum + tdelta
             
-        #     sumInSeconds = float(delaySum.hour * 60 * 60 + delaySum.minute * 60)
-        #     avgDelayInSeconds = float(sumInSeconds) / float(len(lineTrips))
+            sumInSeconds = float(delaySum.hour * 60 * 60 + delaySum.minute * 60)
+            avgDelayInSeconds = float(sumInSeconds) / float(len(lineTrips))
 
-        #     secondsToAdd = timedelta(seconds=avgDelayInSeconds)
-        #     secondsToAddInDatetime = datetime(2000, 1, 1, 0, 0, 0) + secondsToAdd
-        #     HMSFormat='%H:%M:%S'
-        #     result[line] = secondsToAddInDatetime.strftime(HMSFormat)
+            secondsToAdd = timedelta(seconds=avgDelayInSeconds)
+            secondsToAddInDatetime = datetime(2000, 1, 1, 0, 0, 0) + secondsToAdd
+            HMSFormat='%H:%M:%S'
+            result[line] = secondsToAddInDatetime.strftime(HMSFormat)
 
-        # print("Result to be added to db: ") 
-        # print(result)
-        # today=datetime.today().strftime('%Y-%m-%d')
-        # self.statisticsRepository.delete_line_statistics_by_day(today)
-        # self.statisticsRepository.insert_line_statistics(result, today) 
+        print("Result to be added to db (LINES): ") 
+        print(result)
+        print("Result to be added to db (STOPS): ") 
+        print(stop_result)
+        today=datetime.today().strftime('%Y-%m-%d')
+        self.statisticsRepository.delete_line_statistics_by_day(today)
+        self.statisticsRepository.insert_line_statistics(result, stop_result, today) 

481f33e4f66d3b0c4bcfe6a0b1e61d88c9380dba
Szymon <ctk7000@gmail.com>
2020-06-29 08:28:59 +0200

    ADDED COUNT for stops

--- AzureFunctions/ArchiveManager/ArchiveService.py
+++ AzureFunctions/ArchiveManager/ArchiveService.py
@@ -17,15 +17,27 @@ class ArchiveService:
         all_stops = self.tripRepository.get_all_stops(currentDay)
         print(all_stops)
         stop_result = {}
+        max_delay_stop_name = ""
+        max_delay_date_time = timedelta(seconds=0)
+        max_stop_trip_count = 0
+        max_stop_trip_name = ""
         for stop in all_stops:
             print("Current stop" + stop) 
+            trip_count = 0
             stopTrips = self.tripRepository.get_trips_for_stop(stop, currentDay)
             delaySum = datetime.strptime("00:00", FMT)
             for trip in stopTrips:
+                trip_count = trip_count + 1
                 planned_time = trip["planned_time"]
                 actual_time = trip["actual_time"]
                 tdelta = datetime.strptime(actual_time, FMT) - datetime.strptime(planned_time, FMT)
+                if(tdelta.total_seconds() > max_delay_date_time.total_seconds()):
+                    max_delay_date_time = tdelta
+                    max_delay_stop_name = stop.replace('.', "")
                 delaySum = delaySum + tdelta
+            if (trip_count > max_stop_trip_count):
+                max_stop_trip_count = trip_count
+                max_stop_trip_name = stop.replace('.', "")        
             print("DelaySum: ")
             print(delaySum)  
             sumInSeconds = float(delaySum.hour * 60 * 60 + delaySum.minute * 60)
@@ -69,4 +81,4 @@ class ArchiveService:
         print(stop_result)
         today=datetime.today().strftime('%Y-%m-%d')
         self.statisticsRepository.delete_line_statistics_by_day(today)
-        self.statisticsRepository.insert_line_statistics(result, stop_result, today) 
+        self.statisticsRepository.insert_line_statistics(result, stop_result, max_delay_stop_name, max_delay_date_time.total_seconds(), max_stop_trip_name, max_stop_trip_count, today) 
--- AzureFunctions/ArchiveManager/repository/StatisticsRepository.py
+++ AzureFunctions/ArchiveManager/repository/StatisticsRepository.py
@@ -11,11 +11,15 @@ class StatisticsRepository:
         load_dotenv()
         self.connection_string = os.getenv("mongo_connection")
 
-    def insert_line_statistics(self, lines_with_avg_delay_dict, stops_with_avg_delay_dict, day):
+    def insert_line_statistics(self, lines_with_avg_delay_dict, stops_with_avg_delay_dict, max_delay_stop_name, max_delay_date_time, max_stop_trip_name, max_stop_trip_count, day):
         client = MongoClient(self.connection_string)
         db = client['my-db']
         collection = db['statistics']
-        collection.insert_one({"timestamp" :  day, "avg_delay_line_statistics" : lines_with_avg_delay_dict, "avg_delay_stop_statistics" : stops_with_avg_delay_dict})
+        collection.insert_one({"timestamp" :  day, "avg_delay_line_statistics" : lines_with_avg_delay_dict,
+         "avg_delay_stop_statistics" : stops_with_avg_delay_dict , "max_delay_stop": {"stop_name" : max_delay_stop_name, "delay_in_seconds" : max_delay_date_time},
+         "max_stop_trip_count" : {"stop_name": max_stop_trip_name, "trip_count" : max_stop_trip_count } 
+         
+         })
 
     def delete_line_statistics_by_day(self, day):
         client = MongoClient(self.connection_string)

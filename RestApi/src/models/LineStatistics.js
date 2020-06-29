e3d68972b87599f642a7f913fcde1d83ac036c0a
Szymon Smykała <ctk7000@gmail.com>
2020-06-29 16:23:02 +0200

    Add max departures (#27)
    * ADDED COUNT for stops
    
    * added rest api for daily stats
    
    * reverted crawler cron
    
    * reverted running crawler service
    
    * removed package lock
    
    * added to ignore

--- RestApi/src/models/LineStatistics.js
+++ RestApi/src/models/LineStatistics.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 
+
 const RoomSchema = mongoose.Schema({
     timestamp: String,
     avg_delay_line_statistics: Object,
-    avg_delay_stop_statistics: Object
+    avg_delay_stop_statistics: Object,
+    max_delay_stop: {
+        stop_name: String,
+        delay_in_seconds: Number,
+    },
+    max_stop_trip_count:{
+        stop_name: String,
+        trip_count: Number,
+    },
+    max_delay_line:{
+        line_name: String,
+        delay_in_seconds: Number,
+    },
+    max_line_trip_count: {
+        line_name: String,
+        trip_count: Number
+    }
 });
 
 module.exports = mongoose.model('statistics', RoomSchema);
